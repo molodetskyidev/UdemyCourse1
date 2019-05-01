4f3067435598d6316f1814384767b1acb12f537a
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2019-05-01 07:21:26 +0300

    if

--- src/Hello.java
+++ src/Hello.java
@@ -6,11 +6,17 @@ public static void main(String [] args) {
 	int value=0;
 	while (value<5) {
 	System.out.println(value+". Hello, " + myName);
+	if (value==3) {
+		System.out.println("OK");
+	} else {
+		System.out.println("000000000000000000000000");	
+	}
 	value=value+1;
 	}
 	System.out.println("----------------------------");
 	for(int i=0;i<5;i++) {
 		System.out.println(i+". Hello, " + myName);
 	}
+	
 }
 }
