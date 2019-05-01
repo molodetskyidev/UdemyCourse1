6d33261ced05f6462283af019a1278bb025e6350
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2019-05-01 06:58:52 +0300

    tutorial 2

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,8 @@
  
 public class Hello {
 public static void main(String [] args) {
-	
-	System.out.print("Hello world");
+	String myName;
+	myName="Alex";
+	System.out.print("Hello, " + myName);
 }
 }
